3cf80be3369cf7bc72f75d87d69dcbf0264ac7c0
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-22 12:38:17 +0530

    Binary operator uses this code

---
+++ binary.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Binary operator
+//cout<<4*13.2;//print high precedures int *float=float
+//cout<<132*4;//print high precedures int *float=float
+//cout<<13.2/4;// print high precedures float/=float
+//cout<<4/4;//print high precedures int /int=int
+//cout<<18/4;//print high precedures int/int=int only write decimal value
+cout<<2*3-4/2;// use Associativity rule in left right // {*, /, + , -}>{+,-}
+
+}
