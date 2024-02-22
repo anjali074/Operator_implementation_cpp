1a81a9aa1b2f1a70ca968f469f70ba4c580ede97
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-22 12:26:45 +0530

    README.md

---
+++ unary.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // unary operator
+  int a=15;
+  //int b=a++;
+ // int b=++a;
+ //int b=a--;
+int b=--a;
+  //cout<<a++;// post increment
+  //cout<<++a;//pre increment
+  //cout<<a--;// post decrement
+  //cout<<--a;//pre decrement
+cout<<b<<" "<<a ;
+  cout<<endl<<a;
+  return 0;
+}
