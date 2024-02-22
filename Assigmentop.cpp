0dd43758bbd1fa0b33bceee950d828d660b9ea39
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-22 13:30:52 +0530

    README.md

---
+++ Assigmentop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a=10;
+  a+=15;//a=a+15
+  cout<<a;
+  /*a-=20;//a=a-20
+  cout<<a;
+  a*=3;//a=a*3
+  cout<<a;
+  a%=5;//a=a%5
+  cout<<a;
+  a/6;//a=a/6
+  cout<<a;*/
+
+}
