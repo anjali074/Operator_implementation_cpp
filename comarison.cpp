ff7ce1e176e0e8bb8b4fb8c6d151b9586b23fc9c
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-22 12:54:09 +0530

    Add the comparision operator all use

---
+++ comarison.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //comparision operator
+  //{==,>, <, >=, <=, !=}
+  int a,b;
+  cout<<"enter the value a"<<endl;
+  cin>>a;
+  cout<<"enter the value b"<<endl;
+  cin>>b;
+  //if(a==b){// equal operator
+   // if(a<b){// less than
+    //if(a>b){// greator operator
+    //if(a>=b){// greater than equal to
+      //if(a<=b){// less than equal to
+        if(a!=b){// not equal to
+    cout<<"yes";
+  }
+  else{
+    cout<<"No";
+    return 0;
+  }
+  }
