677133a2684aebcc449e8c1b966d8418ce0f25f7
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-22 13:24:08 +0530

    Add bitwise operator using this implement

---
+++ Bitwise.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Bitwise operator
+  int ans=2^3;//EX-OR
+  cout<<ans<<endl;
+  int a=2|3;
+  cout<<a<<endl;
+  int b=2<<3;//left shift
+  cout<<b<<endl;
+  int c=2&3;//AND
+  cout<<c<<endl;
+  int d=~2;//complement
+  cout<<d<<endl;
+  int e=2<<5;
+  cout<<e;   
+}
