f97d894e57846d8ecb54910ea2d284eae84b975d
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-24 22:18:59 +0530

    add increment or ecrement

---
+++ inc.dre.cpp
@@ -0,0 +1,11 @@
+//print increment increment or decrement
+#include<iostream>
+using namespace std;
+int main(){
+  int i=7;
+  cout<<++i<<endl;
+  cout<<i++<<endl;
+  cout<<i--<<endl;
+  cout<<--i<<endl;
+  return 0;
+}
